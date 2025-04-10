9690e6289b6e9d799744d5f6de83f39cf878dece
Damian Elenbaas <damianelenbaas2003@gmail.com>
2025-04-10 14:43:45 +0200

    feat: Added boilerplate code

---
+++ cmd/version.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of WP Plugin Generator",
+	Long:  `All software has versions. This is WP Plugin Generator's`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("WP Plugin Generator v0.1 -- HEAD")
+	},
+}
