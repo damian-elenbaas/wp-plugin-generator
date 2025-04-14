a580537ce58ae5a030b643dc2b647bfa92b9fdbb
Damian Elenbaas <damianelenbaas2003@gmail.com>
2025-04-14 18:13:36 +0200

    feat: Added misc include files

--- internal/generator/php_files.go
+++ internal/generator/php_files.go
@@ -168,47 +168,54 @@ func generateSlug(projectName string) (*string, error) {
 }
 
 func (data *pluginData) createFiles(exportDir string) error {
-	err := data.createPluginRootFile(exportDir)
-	if err != nil {
-		return err
-	}
-
-	err = data.createMainFile(exportDir)
-	if err != nil {
-		return err
-	}
-
-	err = data.createLoaderFile(exportDir)
-	if err != nil {
-
-	}
+	data.createPluginRootFile(exportDir)
+	data.createMainFile(exportDir)
+	data.createLoaderFile(exportDir)
+	data.createActivatorFile(exportDir)
+	data.createDeactivatorFile(exportDir)
+	data.createI18nFile(exportDir)
 
 	return nil
 }
 
-func (data *pluginData) createPluginRootFile(exportDir string) error {
+func (data *pluginData) createPluginRootFile(exportDir string) {
 	processTemplate(
 		"plugin-name.php.tmpl",
 		filepath.Join(exportDir, data.Slug+".php"),
 		*data)
-
-	return nil
 }
 
-func (data *pluginData) createMainFile(exportDir string) error {
+func (data *pluginData) createMainFile(exportDir string) {
 	processTemplate(
 		"includes/PluginName.php.tmpl",
 		filepath.Join(exportDir, "includes/"+data.ClassPrefix+".php"),
 		*data)
-
-	return nil
 }
 
-func (data *pluginData) createLoaderFile(exportDir string) error {
+func (data *pluginData) createLoaderFile(exportDir string) {
 	processTemplate(
 		"includes/PluginName_Loader.php.tmpl",
 		filepath.Join(exportDir, "includes/"+data.ClassPrefix+"_Loader.php"),
 		*data)
+}
 
-	return nil
+func (data *pluginData) createActivatorFile(exportDir string) {
+	processTemplate(
+		"includes/PluginName_Activator.php.tmpl",
+		filepath.Join(exportDir, "includes/"+data.ClassPrefix+"_Activator.php"),
+		*data)
+}
+
+func (data *pluginData) createDeactivatorFile(exportDir string) {
+	processTemplate(
+		"includes/PluginName_Deactivator.php.tmpl",
+		filepath.Join(exportDir, "includes/"+data.ClassPrefix+"_Deactivator.php"),
+		*data)
+}
+
+func (data *pluginData) createI18nFile(exportDir string) {
+	processTemplate(
+		"includes/PluginName_i18n.php.tmpl",
+		filepath.Join(exportDir, "includes/"+data.ClassPrefix+"_i18n.php"),
+		*data)
 }
